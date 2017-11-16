393f7731dc5c6310e1defff74f58e064dc7cca19
Hola Nguyen <kevinhoa95@mgail.com>
2017-11-16 14:02:09 +0700

    fix learning rate

--- densenet_train_age.py
+++ densenet_train_age.py
@@ -96,7 +96,7 @@ with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
 
     epoch_learning_rate = init_learning_rate
     for epoch in range(1, total_epochs + 1):
-        if epoch == (total_epochs * 0.25) or epoch == (total_epochs * 0.5) or epoch == (total_epochs * 0.75):
+        if epoch == (total_epochs * 0.2) or epoch == (total_epochs * 0.4) or epoch == (total_epochs * 0.6) or epoch == (total_epochs * 0.8):
             epoch_learning_rate = epoch_learning_rate / 10
 
         pre_index = 0
