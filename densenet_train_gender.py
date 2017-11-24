143cc372c7aa0dc6e7359729c56ab4484ac611d7
Hola Nguyen <kevinhoa95@mgail.com>
2017-11-24 09:19:03 +0700

    fix model for new data

--- densenet_train_gender.py
+++ densenet_train_gender.py
@@ -7,6 +7,7 @@ import numpy as np
 from utils import *
 from parameters import *
 from densenet import *
+import time
 
 
 def Evaluate(sess, test_x, test_y):
@@ -63,7 +64,16 @@ learning_rate = tf.placeholder(tf.float32, name='learning_rate')
 logits = DenseNet(x=x, nb_blocks=nb_block, filters=growth_k, training=training_flag).model
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=logits), name="cost")
 
-l2_loss = tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables()])
+# l2_loss = tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables()])
+# l2 weight decay loss
+costs = []
+for var in tf.trainable_variables():
+    costs.append(tf.nn.l2_loss(var))
+    tf.summary.histogram(var.op.name, var)
+l2_loss = tf.add_n(costs)
+
+# regular_loss = cost + l2_loss * weight_decay
+
 optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=nesterov_momentum, use_nesterov=True, name="optimizer")
 train = optimizer.minimize(cost + l2_loss * weight_decay, name='train')
 
@@ -89,67 +99,83 @@ saver = tf.train.Saver(tf.global_variables())
 # Start train
 print("Modeling done, starting training...")
 with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
-    ckpt = tf.train.get_checkpoint_state('./model-gender')
+
+    ckpt = tf.train.get_checkpoint_state('./model-gender-new')
     if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
         saver.restore(sess, ckpt.model_checkpoint_path)
     else:
         sess.run(tf.global_variables_initializer())
 
-    summary_writer = tf.summary.FileWriter('./logs-gender', sess.graph)
-
+    #start input enqueue threads
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess= sess, coord=coord)
     epoch_learning_rate = init_learning_rate
-    for epoch in range(1, total_epochs + 1):
-        if epoch == (total_epochs * 0.2) or epoch == (total_epochs * 0.4) or epoch == (total_epochs * 0.6) or epoch == (total_epochs * 0.8):
-            epoch_learning_rate = epoch_learning_rate / 10
+    summary_writer = tf.summary.FileWriter('./logs-gender-new', sess.graph)
+    try:
+        while not coord.should_stop():
+            for epoch in range(1, total_epochs + 1):
+                if epoch == (total_epochs * 0.2) or epoch == (total_epochs * 0.4) or epoch == (total_epochs * 0.6) or epoch == (total_epochs * 0.8):
+                    epoch_learning_rate = epoch_learning_rate / 10
 
-        pre_index = 0
-        train_acc = 0.0
-        train_loss = 0.0
+                pre_index = 0
+                train_acc = 0.0
+                train_loss = 0.0
+                iteration = int(train_y.shape[0] / batch_size)
 
-        iteration = int(train_y.shape[0] / batch_size)
-        for step in range(iteration):
-            if pre_index + batch_size < train_y.shape[0]:
-                batch_x = train_x[pre_index: pre_index + batch_size]
-                batch_y = train_y[pre_index: pre_index + batch_size]
-            else:
-                batch_x = train_x[pre_index:]
-                batch_y = train_y[pre_index:]
+                # start time of epoch
+                start_time = time.time()
+                for step in range(iteration):
+                    if pre_index + batch_size < train_y.shape[0]:
+                        batch_x = train_x[pre_index: pre_index + batch_size]
+                        batch_y = train_y[pre_index: pre_index + batch_size]
+                    else:
+                        batch_x = train_x[pre_index:]
+                        batch_y = train_y[pre_index:]
 
-            # randomize data
-            batch_x = data_augmentation(batch_x)
+                    # randomize data
+                    batch_x = data_augmentation(batch_x)
 
-            train_feed_dict = {
-                x: batch_x,
-                label: batch_y,
-                learning_rate: epoch_learning_rate,
-                training_flag: True
-            }
+                    train_feed_dict = {
+                        x: batch_x,
+                        label: batch_y,
+                        learning_rate: epoch_learning_rate,
+                        training_flag: True
+                    }
+                    _, batch_loss = sess.run([train, cost], feed_dict=train_feed_dict)
+                    batch_acc = accuracy.eval(feed_dict=train_feed_dict)
 
-            _, batch_loss = sess.run([train, cost], feed_dict=train_feed_dict)
-            batch_acc = accuracy.eval(feed_dict=train_feed_dict)
+                    train_loss += batch_loss
+                    train_acc += batch_acc
+                    pre_index += batch_size
 
-            train_loss += batch_loss
-            train_acc += batch_acc
-            pre_index += batch_size
+                    # Buoc step phai so sanh voi iteration - 1
+                    if step == iteration - 1:
+                        train_loss /= iteration  # average loss
+                        train_acc /= iteration  # average accuracy
 
-            # Buoc step phai so sanh voi iteration - 1
-            if step == iteration - 1:
-                train_loss /= iteration  # average loss
-                train_acc /= iteration  # average accuracy
+                        train_summary = tf.Summary(value=[tf.Summary.Value(tag='train_loss', simple_value=train_loss),
+                                                          tf.Summary.Value(tag='train_accuracy', simple_value=train_acc)])
+                        tf.add_to_collection('train_summary', train_summary)
+                        test_acc, test_loss, test_summary = Evaluate(sess, test_x, test_y)
 
-                train_summary = tf.Summary(value=[tf.Summary.Value(tag='train_loss', simple_value=train_loss),
-                                                  tf.Summary.Value(tag='train_accuracy', simple_value=train_acc)])
-                tf.add_to_collection('train_summary', train_summary)
-                test_acc, test_loss, test_summary = Evaluate(sess, test_x, test_y)
+                        dur_time = time.time() - start_time
+                        summary_writer.add_summary(summary=train_summary, global_step=epoch)
+                        summary_writer.add_summary(summary=test_summary, global_step=epoch)
+                        # summary_writer.flush()
 
-                summary_writer.add_summary(summary=train_summary, global_step=epoch)
-                summary_writer.add_summary(summary=test_summary, global_step=epoch)
-                # summary_writer.flush()
+                        log_line = "epoch: %d/%d, train_loss: %.4f, train_acc: %.4f, test_loss: %.4f, test_acc: %.4f, time: %ss \n" % (
+                            epoch, total_epochs, train_loss, train_acc, test_loss, test_acc, str(dur_time))
+                        print(log_line)
+                        with open('logs-gender.txt', 'a') as f:
+                            f.write(log_line)
+                saver.save(sess=sess, save_path='./model-gender-new/dense.ckpt')
+    except tf.errors.OutOfRangeError as e:
+        with open('logs_out_of_range.txt', 'a') as f:
+            f.write(e)
+    finally:
+        # When done, ask the threads to stop.
+        coord.request_stop()
 
-                log_line = "epoch: %d/%d, train_loss: %.4f, train_acc: %.4f, test_loss: %.4f, test_acc: %.4f \n" % (
-                    epoch, total_epochs, train_loss, train_acc, test_loss, test_acc)
-                print(log_line)
-
-                with open('logs-gender.txt', 'a') as f:
-                    f.write(log_line)
-        saver.save(sess=sess, save_path='./model-gender/dense.ckpt')
+    # Wait for threads to finish.
+    coord.join(threads)
+    sess.close()
