e3353a89040ee7ca7c70015371105277281a6601
Hoa Nguyen <kevinhoa95@gmail.com>
2018-01-15 17:09:06 +0700

    unhashable type list

--- densenet_ages_with_queue.py
+++ densenet_ages_with_queue.py
@@ -16,7 +16,7 @@ def Evaluate(sess):
     accuracy = tf.get_collection('accuracy')
     epoch_learning_rate = tf.get_collection('epoch_learning_rate')
     learning_rate = tf.get_collection('learning_rate')
-    training_flag = tf.get_collection('training_flag')
+    # training_flag = tf.get_collection('training_flag')
 
     test_feed_dict = {
         learning_rate: epoch_learning_rate,
